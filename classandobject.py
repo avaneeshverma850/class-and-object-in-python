2b2e7d8abc5efae7c3ca24bff486ea01ba43813e
Avaneesh verma <124239728+avaneeshverma850@users.noreply.github.com>
2024-01-03 23:54:50 +0530

    Add files via upload

---
+++ classandobject.py
@@ -0,0 +1,20 @@
+#Syntax of class 
+#write a class keyword
+#class definition must be executed before its use.
+""" objects-
+Three types of objects-
+1-Instance objects-
+2-class objects
+3-function object"""
+#Creating instance object
+class test:
+    #variables
+    #3-function
+    x1=5
+    x2=3
+    def f1():
+        print("Hello")
+t1=test()
+t2=test()
+print(type(t1))
+    
